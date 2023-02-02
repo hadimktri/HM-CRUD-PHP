1fb7233ae238e88d70018382d1425648d4e0840e
hadimktri <116309036+hadimktri@users.noreply.github.com>
2023-02-02 10:37:47 -0800

    Update navigation.php

--- layout/navigation.php
+++ layout/navigation.php
@@ -18,30 +18,34 @@
   </ul>
 
   <ul class="nav me-2">
-    
+    <?php if (!isset($_SESSION["logged_in"])) : ?>
 
-  <li class="nav-item p-1" >
-    <a class="nav-link text-white" href="login.php" target=blank>Sign In</a>
-  </li>
+      <li class="nav-item p-1">
+        <a class="nav-link text-white" href="login.php" target=blank>Sign In</a>
+      </li>
 
-  <li class="nav-item p-1">
-    <a class="nav-link text-white" href="new_user.php" target=blank>Sign Up</a>
-  </li>
+      <li class="nav-item p-1">
+        <a class="nav-link text-white" href="new_user.php" target=blank>Sign Up</a>
+      </li>
 
+    <?php endif; ?>
 
-  <li class="nav-item p-1">
-    <a class="nav-link text-white" href="logout.php" target=blank>Sign Out</a>
-  </li>
+    <?php if (isset($_SESSION["logged_in"])) : ?>
 
-  <li class="nav-item p-1">
-    <a class="nav-link text-white" href="edit_profile.php" target=blank>Edit Profile</a>
-  </li>
+      <li class="nav-item p-1">
+        <a class="nav-link text-white" href="logout.php" target=blank>Sign Out</a>
+      </li>
 
-  <li class="nav-item">
-    <a target=blank class="nav-link text-white" href="profile.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-      </svg></a>
-  </li>
+      <li class="nav-item p-1">
+        <a class="nav-link text-white" href="edit_profile.php" target=blank>Edit Profile</a>
+      </li>
 
+      <li class="nav-item">
+        <a target=blank class="nav-link text-white" href="profile.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+          </svg></a>
+      </li>
+
+    <?php endif; ?>
   </ul>
 </nav>
